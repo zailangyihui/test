b79a1bb7006a5b63e8031b4596cf9ef0f1fdd9e4
rongwozailangyihui <1134115389@qq.com>
2018-06-13 16:54:03 +0800

    删除Home.vue文件，开放菜单管理页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 //common module
-import Login    from '@/common/login'
+import Login from '@/common/login'
 import Register from '@/common/register'
-import Home     from '@/common/home'
-import About    from '@/common/about'
+import Home from '@/common/home'
+import About from '@/common/about'
 
 //custom module
 //系统管理
@@ -16,36 +16,35 @@ import menuManager from '@/views/system/menuManager'
 import blackList from '@/views/system/blackList'
 import displaySetting from '@/views/system/displaySetting'
 
-import {getCookie} from '@/utils/Cookie'
+import { getCookie } from '@/utils/Cookie'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/Register',
-      name: 'Register',
-      component: Register
-    },
-    {
-	    path: '/Home',
-	    component: Home,
-	    name: '系统管理',
-	    iconCls: 'el-icon-message',//图标样式class
-	    children: [
-	    	{ path: '/about.go', component: About, name: '关于页面' },
-	        { path: '/sys_accountInfo.go', component: accountInfo, name: '用户信息' },
-	        { path: '/sys_logsList.go', component: logsList, name: '日志查询' },
-	        { path: '/sys_menuManager.go', component: menuManager, name: '菜单管理' },
-	        { path: '/sys_blackList.go', component: blackList, name: '黑名单' },
-	        { path: '/sys_displaySetting.go', component: displaySetting, name: '显示设置' }, 
-	        { path: '/sys_userManager.go', component: userManager, name: '用户管理' }, 
-	    ],
-    },
-  ]
+    routes: [{
+            path: '/',
+            name: 'Login',
+            component: Login
+        },
+        {
+            path: '/Register',
+            name: 'Register',
+            component: Register
+        },
+        {
+            path: '/Home',
+            component: Home,
+            name: '系统管理',
+            iconCls: 'el-icon-message', //图标样式class
+            children: [
+                { path: '/about.go', component: About, name: '关于页面' },
+                { path: '/sys_accountInfo.go', component: accountInfo, name: '用户信息' },
+                { path: '/sys_logsList.go', component: logsList, name: '日志查询' },
+                { path: '/sys_menuManager.go', component: menuManager, name: '菜单管理' },
+                { path: '/sys_blackList.go', component: blackList, name: '黑名单' },
+                { path: '/sys_displaySetting.go', component: displaySetting, name: '显示设置' },
+                { path: '/sys_userManager.go', component: userManager, name: '用户管理' },
+            ],
+        },
+    ]
 })
